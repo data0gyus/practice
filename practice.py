4107415beed3533acf9ce31fd21096c2e4831155
data0gyus <dwjsk2008@gmail.com>
2025-01-13 17:12:33 +0900

    프로그래머스 옷가게 할인받기

--- practice.py
+++ practice.py
@@ -2,9 +2,17 @@
 
 for n in range(N):
     print("123")
+
+
 """
 
 
-def solution(n):
-    answer = 0
-    return answer
+def solution(price):
+    if price >= 500000:
+        return int(price*0.8)
+    elif price >= 300000 and price < 500000:
+        return int(price * 0.9)
+    elif price >= 100000 and price < 300000:
+        return int(price * 0.95)
+    else:
+        return int(price)
