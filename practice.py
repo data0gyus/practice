6135ac527cd5414e5646c8dea23f168981527e41
data0gyus <dwjsk2008@gmail.com>
2025-01-19 16:37:52 +0900

    01.19 프로그래머스 코딩 테스트 LV.0 3페이지 끝

--- practice.py
+++ practice.py
@@ -393,3 +393,20 @@ def solution(my_string, letter):
         if string != letter:
             answer += string
     return answer
+
+
+# 홀짝 구분하기
+a = int(input())
+if a % 2 == 0:
+    print(a, 'is even')
+else:
+    print(a, 'is odd')
+
+a = int(input())
+if a % 2 == 0:
+    print(f'{a} is even')
+else:
+    print(f'{a} is odd')
+
+a = int(input())
+print(f"{a} is odd" if a % 2 else f"{a} is even")
